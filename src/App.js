7a364f7909dd1fe6aaff52abc349d92c0e697fa9
WahyuS002 <syahputrawahyu61@gmail.com>
2022-04-22 13:34:34 +0700

    :zap: small layout fix

--- src/App.js
+++ src/App.js
@@ -11,16 +11,18 @@ function App() {
         <div className="font-rubik min-h-screen bg-black text-white">
             <ToastContainer />
             <div className="absolute inset-0 bg-[url('./assets/images/banner.png')] bg-no-repeat md:bg-cover bg-top w-full min-h-screen"></div>
-            <div className="mx-20 py-12">
-                <Navbar />
-            </div>
-            <div className="mx-5 md:mx-20 my-12">
-                <Minting />
+            <div className="min-h-screen">
+                <div className="mx-20 py-12">
+                    <Navbar />
+                </div>
+                <div className="mx-5 md:mx-20 my-12">
+                    <Minting />
+                </div>
             </div>
             {/* <div className="mt-12">
                 <MovementImages />
             </div> */}
-            <div className="mx-5 md:mx-20 md:pt-28 pt-20">
+            <div className="mx-5 md:mx-20 -mt-40 md:mt-12">
                 <Roadmap />
             </div>
         </div>
